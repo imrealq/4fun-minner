b9ce8d364475cf1607dff19c3fcae0c9f82508ee
imrealq <quynhtn29@gmail.com>
2024-04-08 22:36:15 +0700

    install docker

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 apt-get update
 
 apt-get install git ca-certificates curl tmux -y
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
